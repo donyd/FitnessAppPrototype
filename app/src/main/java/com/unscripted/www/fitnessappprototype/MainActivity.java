60b438dc5ba3704f0e4027191ef3aecd20d5590e
= <=>
2017-03-02 22:53:44 +0000

    Added new activity and button to launch to

--- app/src/main/java/com/unscripted/www/fitnessappprototype/MainActivity.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/MainActivity.java
@@ -1,7 +1,10 @@
 package com.unscripted.www.fitnessappprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button launchActivityTwoButton = (Button) findViewById(R.id.button);
+        launchActivityTwoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO:
+                // Launch Activity Two
+                // Hint: use Context's startActivity() method
+
+                // Create an intent stating which Activity you would like to
+                // start
+                // Intent intent = new Intent(this, ActivityTwo.class);
+                //Intent intent = new Intent(android.content.Intent, ActivityTwo);
+                Intent intent = new Intent(MainActivity.this, Session.class);
+
+                // Launch the Activity using the intent
+                startActivity(intent);
+
+            }
+        });
     }
 }
