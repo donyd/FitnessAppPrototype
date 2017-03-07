7d5109d2348c21f4988b649641a9adc160abd7c8
= <=>
2017-03-07 23:36:44 +0000

    Added toggle buttons for levels

--- app/src/main/java/com/unscripted/www/fitnessappprototype/MainActivity.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/MainActivity.java
@@ -5,13 +5,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare ToggleButtons instances
+    ToggleButton beginner, novice, advanced;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize ToggleButtons
+        beginner = (ToggleButton) findViewById(R.id.toggleBeginner);
+        novice = (ToggleButton) findViewById(R.id.toggleNovice);
+        advanced = (ToggleButton) findViewById(R.id.toggleAdvanced);
+
+        beginner.setChecked(true);
+        beginner.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    beginner.setTextOn("Beginner");
+
+                    novice.setChecked(false);
+                    novice.setTextOff("");
+
+                    advanced.setChecked(false);
+                    advanced.setTextOff("");
+                }
+            }
+        });
+
+        novice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    beginner.setChecked(false);
+                    beginner.setTextOff("");
+
+                    novice.setTextOn("Novice");
+
+                    advanced.setChecked(false);
+                    advanced.setTextOff("");
+                }
+            }
+        });
+
+        advanced.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    beginner.setChecked(false);
+                    beginner.setTextOff("");
+
+                    novice.setChecked(false);
+                    novice.setTextOff("");
+
+                    advanced.setTextOn("Advanced");
+                }
+            }
+        });
+
+
 
         Button launchActivityTwoButton = (Button) findViewById(R.id.button);
         launchActivityTwoButton.setOnClickListener(new View.OnClickListener() {
