ddb91258c43cae1762b48e0c84b19b2238f7e25c
karijeru <karina.jerusalmi@gmail.com>
2017-03-11 15:43:51 +0000

    Linked all pages: home, workout, profile

--- app/src/main/java/com/unscripted/www/fitnessappprototype/MainActivity.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/MainActivity.java
@@ -3,14 +3,21 @@ package com.unscripted.www.fitnessappprototype;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     // Declare ToggleButtons instances
     ToggleButton beginner, novice, advanced;
+
+    //Declare Home, Workout and Profile Buttons
+    Button homeBtn;
+    Button workoutBtn;
+    Button profileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         beginner = (ToggleButton) findViewById(R.id.toggleBeginner);
         novice = (ToggleButton) findViewById(R.id.toggleNovice);
         advanced = (ToggleButton) findViewById(R.id.toggleAdvanced);
+
+        //Initialize Home, Workout and Profile Buttons
+        homeBtn= (Button) findViewById(R.id.button1);
+        workoutBtn= (Button) findViewById(R.id.button2);
+        profileBtn= (Button) findViewById(R.id.button3);
 
         beginner.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             @Override
@@ -62,8 +74,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //Open Workout Activity when Body part Selected
+        ImageButton launchActivityButton = (ImageButton) findViewById(R.id.imageButton);
+        launchActivityButton.setOnClickListener(new View.OnClickListener() {
 
-        Button launchActivityTwoButton = (Button) findViewById(R.id.button);
+            @Override
+            public void onClick(View v) {
+                // TODO:
+                // Launch Activity Two
+                // Hint: use Context's startActivity() method
+
+                // Create an intent stating which Activity you would like to
+                // start
+                // Intent intent = new Intent(this, ActivityTwo.class);
+                //Intent intent = new Intent(android.content.Intent, ActivityTwo);
+                Intent intent = new Intent(MainActivity.this, Workout.class);
+
+                // Launch the Activity using the intent
+                startActivity(intent);
+
+            }
+        });
+        //MENU BUTTONS
+        //Home button is selected and cannot be pressed
+        homeBtn.setPressed(true);
+        homeBtn.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                homeBtn.setPressed(true);
+                workoutBtn.setPressed(false);
+                profileBtn.setPressed(false);
+                return true;
+            }
+        });
+
+
+        //WHen clicked links to WORKOUT page
+        Button launchActivityTwoButton = (Button) findViewById(R.id.button2);
         launchActivityTwoButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -83,5 +129,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        //WHen clicked links to PROFILE page
+        Button launchActivityThreeButton = (Button) findViewById(R.id.button3);
+        launchActivityThreeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO:
+                // Launch Activity Two
+                // Hint: use Context's startActivity() method
+
+                // Create an intent stating which Activity you would like to
+                // start
+                // Intent intent = new Intent(this, ActivityTwo.class);
+                //Intent intent = new Intent(android.content.Intent, ActivityTwo);
+                Intent intent = new Intent(MainActivity.this, Profile.class);
+
+                // Launch the Activity using the intent
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/unscripted/www/fitnessappprototype/Workout.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/Workout.java
@@ -1,13 +1,82 @@
 package com.unscripted.www.fitnessappprototype;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class Workout extends AppCompatActivity {
+
+    //Declare workout button
+    Button workoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout);
+        setContentView(R.layout.activity_workout2);
+
+        //Initalize workout
+        workoutBtn= (Button) findViewById(R.id.button2);
+
+        //Workout button is selected and cannot be pressed
+        workoutBtn.setPressed(true);
+        workoutBtn.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                workoutBtn.setPressed(true);
+                return true;
+            }
+        });
+
+        //When clicked links to HOME page
+        Button launchActivityOneButton = (Button) findViewById(R.id.button1);
+        launchActivityOneButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO:
+                // Launch Activity Two
+                // Hint: use Context's startActivity() method
+
+                // Create an intent stating which Activity you would like to
+                // start
+                // Intent intent = new Intent(this, ActivityTwo.class);
+                //Intent intent = new Intent(android.content.Intent, ActivityTwo);
+                Intent intent = new Intent(Workout.this, MainActivity.class);
+
+                // Launch the Activity using the intent
+                startActivity(intent);
+
+            }
+        });
+
+        //WHen clicked links to PROFILE page
+        Button launchActivityThreeButton = (Button) findViewById(R.id.button3);
+        launchActivityThreeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO:
+                // Launch Activity Two
+                // Hint: use Context's startActivity() method
+
+                // Create an intent stating which Activity you would like to
+                // start
+                // Intent intent = new Intent(this, ActivityTwo.class);
+                //Intent intent = new Intent(android.content.Intent, ActivityTwo);
+                Intent intent = new Intent(Workout.this, Profile.class);
+
+                // Launch the Activity using the intent
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
 }
+
