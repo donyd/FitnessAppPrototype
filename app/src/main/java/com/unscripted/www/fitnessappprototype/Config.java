fcbf9a309e510a4022511e5587354c2b132c44a5
karijeru <karina.jerusalmi@gmail.com>
2017-04-23 20:32:19 +0100

    added config

---
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/Config.java
@@ -0,0 +1,15 @@
+package com.unscripted.www.fitnessappprototype;
+
+/**
+ * Created by Kari on 23/04/2017.
+ * References: https://www.sitepoint.com/using-the-youtube-api-to-embed-video-in-an-android-app/
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyD_V6GCDvchMvlf82kcl0b3LJvVu10NHZs";
+
+}
