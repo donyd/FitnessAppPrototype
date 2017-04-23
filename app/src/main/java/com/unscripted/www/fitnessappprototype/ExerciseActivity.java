bb44f3f2db84d747fb7406c21d8b6ba8b20156c9
karijeru <karina.jerusalmi@gmail.com>
2017-04-23 19:43:15 +0100

    Added video to exercise page

--- app/src/main/java/com/unscripted/www/fitnessappprototype/ExerciseActivity.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/ExerciseActivity.java
@@ -1,58 +1,58 @@
 package com.unscripted.www.fitnessappprototype;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.TransitionDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class ExerciseActivity extends AppCompatActivity {
-    // Reference to views
-    ImageView exerciseImageHolder;
-    //Button exerciseButton;
+public class ExerciseActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
-   /* static int mImages[] = new int[]{
-            R.drawable.lungestart,
-            R.drawable.lungeend
-    };*/
+        private static final int RECOVERY_REQUEST = 1;
+        private YouTubePlayerView youTubeView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-        exerciseImageHolder = (ImageView) findViewById(R.id.exerciseImage);
-        //exerciseButton = (Button) findViewById(R.id.btnExercise);
+            youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+            youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+        }
 
-       /* exerciseButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                ((TransitionDrawable) exerciseImageHolder.getDrawable()).startTransition(1000);
+        @Override
+        public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+            if (!wasRestored) {
+                player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
             }
-        });*/
+        }
 
+        @Override
+        public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+            if (errorReason.isUserRecoverableError()) {
+                errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+            } else {
+                String error = String.format(getString(R.string.player_error), errorReason.toString());
+                Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+            }
+        }
 
-        /**
-         * Animation trial
-         * Revision 1
-         * 29/03/2017
-         *
-         * @reference http://stackoverflow.com/questions/26060182/animated-transition-between-two-imageview
-         *
-         */
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == RECOVERY_REQUEST) {
+                // Retry initialization if user performed a recovery action
+                getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+            }
+        }
 
-
-
-       /* TransitionDrawable exerImage = new TransitionDrawable (new Drawable[]{
-           getResources().getDrawables(mImages[0]),
-                getResources().getDrawables(mImages[1])
-        });*/
-
+        protected Provider getYouTubePlayerProvider() {
+            return youTubeView;
+        }
     }
 
 
-}
+
+
