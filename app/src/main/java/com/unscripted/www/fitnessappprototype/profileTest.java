809a9f395ae20680672a1bbf749c640b0e0949d9
karijeru <karina.jerusalmi@gmail.com>
2017-04-25 19:21:58 +0100

    Allow user to update picture

--- app/src/main/java/com/unscripted/www/fitnessappprototype/profileTest.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/profileTest.java
@@ -1,15 +1,28 @@
 package com.unscripted.www.fitnessappprototype;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import com.joooonho.SelectableRoundedImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /*
   * Reference @http://stackoverflow.com/questions/2416844/how-to-set-custom-title-bar-textview-value-dynamically-in-android **Karina
+  * @http://viralpatel.net/blogs/pick-image-from-galary-android-app/
+  * @http://stackoverflow.com/questions/5309190/android-pick-images-from-gallery
   */
 public class profileTest extends AppCompatActivity {
 
@@ -17,6 +30,7 @@ public class profileTest extends AppCompatActivity {
     Button homeBtn;
     Button workoutBtn;
     Button profileBtn;
+    private static int RESULT_LOAD_IMAGE = 1;
 
     // Creates a back button to go BACKWARDS
     @Override
@@ -40,6 +54,7 @@ public class profileTest extends AppCompatActivity {
         homeBtn= (Button) findViewById(R.id.button1);
         workoutBtn= (Button) findViewById(R.id.button2);
         profileBtn= (Button) findViewById(R.id.button3);
+        //profilePicBtn = (Button) findViewById(R.id.profilePic);
 
             // MENU BUTTONS
 
@@ -97,8 +112,90 @@ public class profileTest extends AppCompatActivity {
                 }
             });
 
+            //Pressing this button lets user select a picture from gallery to insert in their profile
+            Button btnLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+            btnLoadImage.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO:
+                    // Launch Activity Two
+                    //
+                    startActivityForResult(
+                            new Intent(
+                                    Intent.ACTION_PICK,
+                                    android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                            ),
+                            RESULT_LOAD_IMAGE
+                    );
+                }
+                /**
+                public void onClick(View arg0) {
+
+                    Intent i = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                }
+
+                 **/
+
+            });
+
+
+
+    }
+    //This code grabs the image chosen by the user and shows it in the screen
+    //>>>>STILL NOT WORKING AS DESIRED
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        // /Detects request codes
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            SelectableRoundedImageView imageView = (SelectableRoundedImageView) findViewById(R.id.imgView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
 
     }
 
+
+/** >>>>>>THIS IS AN ALTERNATIVE CODE TO THE ONE ABOVE THAT IS ALSO NOT WORKING AS DESIRED YET
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Detects request codes
+        if(requestCode==RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+    **/
 
 }
