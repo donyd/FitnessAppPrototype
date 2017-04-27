21a0b1e46080d12b8baf2fe4424312f2082a0579
karijeru <karina.jerusalmi@gmail.com>
2017-04-27 08:02:09 +0100

    Added ads

--- app/src/main/java/com/unscripted/www/fitnessappprototype/ExerciseActivity.java
+++ app/src/main/java/com/unscripted/www/fitnessappprototype/ExerciseActivity.java
@@ -8,6 +8,8 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 /**
  * References: https://www.sitepoint.com/using-the-youtube-api-to-embed-video-in-an-android-app/ **Karina
@@ -15,18 +17,31 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class ExerciseActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
+        //TO PLAY YOUTUBE VIDEOS
         private static final int RECOVERY_REQUEST = 1;
         private YouTubePlayerView youTubeView;
+
+        //TO PLACE ADDS IN OUR APP
+        private static final String TAG = "MainActivity";
+        private AdView mAdView;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
+            //TO PLACE ADDS IN OUR APP
+            mAdView = (AdView) findViewById(R.id.adView);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+
+
+            //INITIALIZE YOUTUBE VIDEO LINK
             youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
             youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
         }
 
+        //PLAY THE FOLLOWING VIDEO
         @Override
         public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
             if (!wasRestored) {
@@ -34,6 +49,7 @@ public class ExerciseActivity extends YouTubeBaseActivity implements YouTubePlay
             }
         }
 
+        //IF THERE IS AN ERROR WITH THE VIDEO...
         @Override
         public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
             if (errorReason.isUserRecoverableError()) {
@@ -44,6 +60,7 @@ public class ExerciseActivity extends YouTubeBaseActivity implements YouTubePlay
             }
         }
 
+        //RECOVERY REQUEST FOR YOUTUBE VIDEO
         @Override
         protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             if (requestCode == RECOVERY_REQUEST) {
